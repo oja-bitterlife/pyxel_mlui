c06225c983f7389fb7a4255818248b05e867b011
oja-bitterlife <bitter.life.club@gmail.com>
2024-11-24 09:58:09 +0900

    引数名をタグ名にしてみた

--- xmlui_pyxel.py
+++ xmlui_pyxel.py
@@ -13,10 +13,10 @@ test_params = {"name":"world", "age":10}
 # 入力待ち
 # *****************************************************************************
 @ui_worker.update_func("my_ui")
-def my_ui_update(state:UI_STATE, event:UI_EVENT):
+def my_ui_update(my_ui:UI_STATE, event:UI_EVENT):
     # メインメニューを開く
     if "button_a" in event.trg:
-        state.open(ui_template, "menu_command")
+        my_ui.open(ui_template, "menu_command")
 
 
 # コマンドメニュー
@@ -24,55 +24,55 @@ def my_ui_update(state:UI_STATE, event:UI_EVENT):
 # 更新
 # ---------------------------------------------------------
 @ui_worker.update_func("menu_win")
-def menu_win_update(state:UI_STATE, event:UI_EVENT):
-    item_w, item_h = state.attrInt("item_w"), state.attrInt("item_h")
+def menu_win_update(menu_win:UI_STATE, event:UI_EVENT):
+    item_w, item_h = menu_win.attrInt("item_w"), menu_win.attrInt("item_h")
 
     # メニューアイテム取得
-    grid = state.arrangeGridByTag("menu_row", "menu_item", item_w, item_h)
-    cursor = UI_GRID_CURSOR(state.findByTag("menu_cur"), grid).moveByEvent(event.trg, "left", "right", "up", "down")
+    grid = menu_win.arrangeGridByTag("menu_row", "menu_item", item_w, item_h)
+    cursor = UI_GRID_CURSOR(menu_win.findByTag("menu_cur"), grid).moveByEvent(event.trg, "left", "right", "up", "down")
 
     # 選択アイテムの表示
     if "button_a" in event.trg:
         if cursor.selected.attrStr("action") == "speak":
             # メッセージウインドウ表示
-            state.open(ui_template, "win_message")
+            menu_win.open(ui_template, "win_message")
 
     # 閉じる
     if "button_b" in event.trg:
-        state.close(state.id)
+        menu_win.close(menu_win.id)
 
 
 # 描画
 # ---------------------------------------------------------
 @ui_worker.draw_func("menu_win")
-def menu_win_draw(state:UI_STATE):
+def menu_win_draw(menu_win:UI_STATE):
     bg_color = 12
-    frame_color = state.attrInt("frame_color", 7)
-    title  = state.attrStr("title", "")
+    frame_color = menu_win.attrInt("frame_color", 7)
+    title  = menu_win.attrStr("title", "")
 
-    pyxel.rect(state.area.x, state.area.y, state.area.w, state.area.h, bg_color)
-    pyxel.rectb(state.area.x, state.area.y, state.area.w, state.area.h, frame_color)
-    pyxel.rectb(state.area.x+1, state.area.y+1, state.area.w-2, state.area.h-2, frame_color)
+    pyxel.rect(menu_win.area.x, menu_win.area.y, menu_win.area.w, menu_win.area.h, bg_color)
+    pyxel.rectb(menu_win.area.x, menu_win.area.y, menu_win.area.w, menu_win.area.h, frame_color)
+    pyxel.rectb(menu_win.area.x+1, menu_win.area.y+1, menu_win.area.w-2, menu_win.area.h-2, frame_color)
 
     if title:
         str_w = FONT_SIZE*len(title)
-        text_x = state.area.x+(state.area.w-str_w)/2
-        pyxel.rect(text_x,state.area.y, str_w, FONT_SIZE, bg_color)
-        pyxel.text(text_x, state.area.y-2, title, frame_color, font)
+        text_x = menu_win.area.x+(menu_win.area.w-str_w)/2
+        pyxel.rect(text_x,menu_win.area.y, str_w, FONT_SIZE, bg_color)
+        pyxel.text(text_x, menu_win.area.y-2, title, frame_color, font)
 
 @ui_worker.draw_func("menu_item")
-def menu_item_draw(state:UI_STATE):
-    color = state.attrInt("color", 7)
-    pyxel.text(state.area.x, state.area.y, state.text, color, font)
+def menu_item_draw(menu_item:UI_STATE):
+    color = menu_item.attrInt("color", 7)
+    pyxel.text(menu_item.area.x, menu_item.area.y, menu_item.text, color, font)
 
 @ui_worker.draw_func("menu_cur")
-def menu_cur_draw(state:UI_STATE):
-    tri_size = state.attrInt("size", 6)
-    color = state.attrInt("color", 7)
+def menu_cur_draw(menu_cur:UI_STATE):
+    tri_size = menu_cur.attrInt("size", 6)
+    color = menu_cur.attrInt("color", 7)
 
     # カーソル表示
-    x = state.area.x-6
-    y = state.area.y+2
+    x = menu_cur.area.x-6
+    y = menu_cur.area.y+2
     pyxel.tri(x, y, x, y+tri_size, x+tri_size//2, y+tri_size//2, color)
 
 
@@ -81,9 +81,9 @@ def menu_cur_draw(state:UI_STATE):
 # 更新
 # ---------------------------------------------------------
 @ui_worker.update_func("msg_win")
-def msg_win_update(state:UI_STATE, event:UI_EVENT):
-    msg_cur = state.findByTag("msg_cur")
-    msg_text = state.findByTag("msg_text")
+def msg_win_update(msg_win:UI_STATE, event:UI_EVENT):
+    msg_cur = msg_win.findByTag("msg_cur")
+    msg_text = msg_win.findByTag("msg_text")
 
     # 文字列更新
     wrap = msg_text.attrInt("wrap", 1024)
@@ -97,7 +97,7 @@ def msg_win_update(state:UI_STATE, event:UI_EVENT):
         # テキストを表示しきっていたら
         if page.is_finish:
             if page.is_end_page:
-                state.close("menu_command")  # メニューごと閉じる
+                msg_win.close("menu_command")  # メニューごと閉じる
             else:
                 page.nextPage()  # 次のページ
         # テキストがまだ残っていたら
@@ -106,37 +106,35 @@ def msg_win_update(state:UI_STATE, event:UI_EVENT):
 
     # メニューごと閉じる
     if "button_b" in event.trg:
-        state.close("menu_command")
+        msg_win.close("menu_command")
 
 
 # 描画
 # ---------------------------------------------------------
 @ui_worker.draw_func("msg_win")
-def msg_win_draw(state:UI_STATE):
-    frame_color = state.attrInt("frame_color", 7)
-    pyxel.rect(state.area.x, state.area.y, state.area.w, state.area.h, 12)
-    pyxel.rectb(state.area.x, state.area.y, state.area.w, state.area.h, frame_color)
-    pyxel.rectb(state.area.x+1, state.area.y+1, state.area.w-2, state.area.h-2, frame_color)
-    pyxel.rectb(state.area.x+3, state.area.y+3, state.area.w-6, state.area.h-6, frame_color)
+def msg_win_draw(msg_win:UI_STATE):
+    frame_color = msg_win.attrInt("frame_color", 7)
+    pyxel.rect(msg_win.area.x, msg_win.area.y, msg_win.area.w, msg_win.area.h, 12)
+    pyxel.rectb(msg_win.area.x, msg_win.area.y, msg_win.area.w, msg_win.area.h, frame_color)
+    pyxel.rectb(msg_win.area.x+1, msg_win.area.y+1, msg_win.area.w-2, msg_win.area.h-2, frame_color)
+    pyxel.rectb(msg_win.area.x+3, msg_win.area.y+3, msg_win.area.w-6, msg_win.area.h-6, frame_color)
 
 @ui_worker.draw_func("msg_text")
-def msg_text_draw(state:UI_STATE):
-    msg_text = state.findByTag("msg_text")
-
+def msg_text_draw(msg_text:UI_STATE):
     # テキスト表示
-    wrap = state.attrInt("wrap", 1024)
-    text = state.getAnimText("draw_count").bind(test_params, wrap)
+    wrap = msg_text.attrInt("wrap", 1024)
+    text = msg_text.getAnimText("draw_count").bind(test_params, wrap)
     page = text.usePage("page_no", msg_text.attrInt("lines", 1))
 
     for i,text in enumerate(page.split()):
-        pyxel.text(state.area.x, state.area.y+i*FONT_SIZE, text, 7, font)
+        pyxel.text(msg_text.area.x, msg_text.area.y+i*FONT_SIZE, text, 7, font)
 
 @ui_worker.draw_func("msg_cur")
-def msg_cur_draw(state:UI_STATE):
-    tri_size = state.attrInt("size", 6)
-    color = state.attrInt("color", 7)
+def msg_cur_draw(msg_cur:UI_STATE):
+    tri_size = msg_cur.attrInt("size", 6)
+    color = msg_cur.attrInt("color", 7)
 
     # カーソル表示
-    x, y = state.area.x, state.area.y
+    x, y = msg_cur.area.x, msg_cur.area.y
     pyxel.tri(x, y, x+tri_size, y, x+tri_size//2, y+tri_size//2, color)
 
