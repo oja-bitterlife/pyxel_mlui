cb2689408c57e1b899b67ea81d18af810b6b3439
oja-bitterlife <bitter.life.club@gmail.com>
2024-11-28 17:34:39 +0900

    コメント修正

--- xmlui_pyxel/win.py
+++ xmlui_pyxel/win.py
@@ -7,7 +7,9 @@ _WINDOW_SPEED = 16
 def _active_color(state:XUStateRO, color:int):
         return 10 if  state.xmlui.debug and state.xmlui.active_state == state and color == 7 else color
 
-class _Base(XUWinRound):
+# ウインドウ基底
+# *****************************************************************************
+class _BaseRound(XUWinRound):
     DEFAULT_PAT = [7,7,12]
 
     def __init__(self, state:XUStateRO):
@@ -18,7 +20,21 @@ class _Base(XUWinRound):
         self.clip.h = self.update_count*_WINDOW_SPEED
         self.draw_buf(pyxel.screen.data_ptr())
 
-class MenuRO(_Base):
+class _BaseRect(XUWinRect):
+    DEFAULT_PAT = [7,7,12]
+
+    def __init__(self, state:XUStateRO):
+        pat = [_active_color(state, c)  for c in self.DEFAULT_PAT]  # アクティブカラーに
+        super().__init__(state, pat, pyxel.width, pyxel.height)
+
+    def draw_win(self):
+        self.clip.h = self.update_count*_WINDOW_SPEED
+        self.draw_buf(pyxel.screen.data_ptr())
+
+
+# グリッドメニュー付きウインドウ
+# *****************************************************************************
+class MenuRO(_BaseRound):
     def __init__(self, state:XUStateRO, tag_group:str, tag_item:str):
         super().__init__(state)
         self._grid = XUSelectGrid(state, tag_group, tag_item)
@@ -38,13 +54,11 @@ class Menu(MenuRO):
     def selected_item(self):
         return self._grid.selected_item
 
-
-
 # デコレータを用意
 def menu_update_func(xmlui:XMLUI, tag_name:str, tag_group:str, tag_item:str):
     def wrapper(update_func:Callable[[Menu,XUEvent], None]):
         # 登録用関数をジェネレート
-        def update(state:XUState,event:XUEvent):
+        def update(state:XUState, event:XUEvent):
             update_func(Menu(state, tag_group, tag_item), event)
         # 関数登録
         xmlui.set_updatefunc(tag_name, update)
@@ -53,8 +67,9 @@ def menu_update_func(xmlui:XMLUI, tag_name:str, tag_group:str, tag_item:str):
 def menu_draw_func(xmlui:XMLUI, tag_name:str, tag_group:str, tag_item:str):
     def wrapper(draw_func:Callable[[MenuRO,XUEvent], None]):
         # 登録用関数をジェネレート
-        def draw(state:XUStateRO,event:XUEvent):
+        def draw(state:XUStateRO, event:XUEvent):
             draw_func(MenuRO(state, tag_group, tag_item), event)
         # 関数登録
         xmlui.set_drawfunc(tag_name, draw)
     return wrapper
+
