5c96e1f9c9313d8c032ecbb9d0bb2f4d8a77ee45
oja-bitterlife <bitter.life.club@gmail.com>
2024-11-29 18:46:52 +0900

    speedを調整できるように

--- xmlui_pyxel/win.py
+++ xmlui_pyxel/win.py
@@ -5,8 +5,6 @@ from . import xui
 
 # ウインドウ基底
 # *****************************************************************************
-_WINDOW_SPEED = 16
-
 # アクティブカラーにする
 def _active_color(state:XUStateRO, color:int):
         return 10 if  state.xmlui.debug and state.xmlui.active_state == state and color == 7 else color
@@ -14,23 +12,27 @@ def _active_color(state:XUStateRO, color:int):
 class _BaseRound(XUWinRound):
     DEFAULT_PAT = [7,7,12]
 
-    def __init__(self, state:XUStateRO):
+    def __init__(self, state:XUStateRO, speed:float=16):
         pat = [_active_color(state, c)  for c in self.DEFAULT_PAT]  # アクティブカラーに
         super().__init__(state, pat, pyxel.width, pyxel.height)
 
+        self.set_attr("speed", speed)
+
     def draw(self):
-        self.clip.h = self.update_count*_WINDOW_SPEED
+        self.clip.h = int(self.update_count*self.speed)
         self.draw_buf(pyxel.screen.data_ptr())
 
 class _BaseRect(XUWinRect):
     DEFAULT_PAT = [7,7,12]
 
-    def __init__(self, state:XUStateRO):
+    def __init__(self, state:XUStateRO, speed:float=16):
         pat = [_active_color(state, c)  for c in self.DEFAULT_PAT]  # アクティブカラーに
         super().__init__(state, pat, pyxel.width, pyxel.height)
 
+        self.set_attr("speed", speed)
+
     def draw(self):
-        self.clip.h = self.update_count*_WINDOW_SPEED
+        self.clip.h = int(self.update_count*self.speed)
         self.draw_buf(pyxel.screen.data_ptr())
 
 
