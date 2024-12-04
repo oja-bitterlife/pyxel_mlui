bb4415eebfbef7b293c95330771ce2b3fddf0eee
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-04 18:31:30 +0900

    タイトル画面開始

--- samples/DQ/main.py
+++ samples/DQ/main.py
@@ -8,23 +8,31 @@ from .ui_code import xmlui,UI_TEMPLATE,input,select
 # *********************************************************
 pyxel.init(256, 256)
 
+import title
+state = "TITLE"
+
 # Main
 def update(): # フレームの更新処理
     # ゲームの更新コード
+    if state == "TITLE":
+        title.update()
 
-    # ゲームの中でメインメニューを開く
-    xmlui.open_by_event(input.BTN_A, UI_TEMPLATE, "command_menu_win")
+    # # ゲームの中でメインメニューを開く
+    # xmlui.open_by_event(input.BTN_A, UI_TEMPLATE, "command_menu_win")
 
-    dial = xmlui.event.get_state("close", input.Dial)
-    if dial:
-        print(dial)
-    yes_no = xmlui.event.get_state("dial_yes", select.List)
-    if yes_no:
-        print(yes_no)
+    # dial = xmlui.event.get_state("close", input.Dial)
+    # if dial:
+    #     print(dial)
+    # yes_no = xmlui.event.get_state("dial_yes", select.List)
+    # if yes_no:
+    #     print(yes_no)
 
 def draw(): # 描画処理
     # ゲームの描画コード
     pyxel.cls(0)
+
+    if state == "TITLE":
+        title.draw()
 
     # UI描画
     xmlui.check_input_on(pyxel.btn)
---
+++ samples/DQ/title.py
@@ -0,0 +1,7 @@
+# タイトル画面
+
+def update():
+    pass
+
+def draw():
+    pass
