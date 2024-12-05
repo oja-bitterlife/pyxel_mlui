04b2e74f78b392b8d59e86e41db48545183cf75f
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-05 17:13:59 +0900

    メニュー調整中

--- xmlui_core.py
+++ xmlui_core.py
@@ -799,16 +799,14 @@ class XUSelectBase(_XUUtilBase):
     ROOT_TAG = "_xmlui_select_root"
     SELECTED_NO_ATTR = "_xmlui_selected_no"
 
-    def __init__(self, state:XUState, rows:int, items:list[XUSelectItem], item_w_attr:str, item_h_attr:str):
+    def __init__(self, state:XUState, items:list[XUSelectItem], rows:int, item_w:int, item_h:int):
         super().__init__(state, self.ROOT_TAG)
         self._rows = rows
         self._items = items
 
         # 座標設定
-        item_w = state.attr_int(item_w_attr, 0)
-        item_h = state.attr_int(item_h_attr, 0)
         for i,item in enumerate(self._items):
-            item.set_pos(i* item_w, i * item_h)
+            item.set_pos(i % rows * item_w, i // rows * item_h)
 
     @property
     def selected_no(self) -> int:
@@ -855,7 +853,9 @@ class XUSelectGrid(XUSelectBase):
     def __init__(self, state:XUState, item_tag:str, rows_attr:str, item_w_attr:str, item_h_attr:str):
         # 自分の直下のitemだけ回収する
         items = list(filter(lambda state: state.enable and state.tag==item_tag, [XUSelectItem(state.xmlui, element) for element in state._element]))
-        super().__init__(state, state.attr_int(rows_attr, 1), items, item_w_attr, item_h_attr)
+        item_w = state.attr_int(item_w_attr, 0)
+        item_h = state.attr_int(item_h_attr, 0)
+        super().__init__(state, items, state.attr_int(rows_attr, 1), item_w, item_h)
 
     # 入力に応じた挙動一括。変更があった場合はTrue
     def _select_by_event(self, input:set[str], left_event:str, right_event:str, up_event:str, down_event:str, x_wrap:bool, y_wrap:bool) -> bool:
@@ -887,7 +887,10 @@ class XUSelectList(XUSelectBase):
     def __init__(self, state:XUState, item_tag:str, item_w_attr:str, item_h_attr:str):
         # 自分の直下のitemだけ回収する
         items = list(filter(lambda state: state.enable and state.tag==item_tag, [XUSelectItem(state.xmlui, element) for element in state._element]))
-        super().__init__(state, 1, items, item_w_attr, item_h_attr)
+        item_w = state.attr_int(item_w_attr, 0)
+        item_h = state.attr_int(item_h_attr, 0)
+        rows = len(items) if item_w > item_h else 1
+        super().__init__(state, items, rows, item_w, item_h)
   
     # 入力に応じた挙動一括。変更があった場合はTrue
     # 選択リストは通常上下ラップする
