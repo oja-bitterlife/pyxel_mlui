8c4270457de79950c5440e107a0b49b062a7e942
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-15 15:27:43 +0900

    select修正

--- xmlui/lib/select.py
+++ xmlui/lib/select.py
@@ -2,8 +2,8 @@ from xmlui.core import *
 
 # セレクトアイテム
 class Item(XUSelectItem):
-    def __init__(self, state:XUElem):
-        super().__init__(state)
+    def __init__(self, elem:XUElem):
+        super().__init__(elem)
 
 # グリッドメニュー付きウインドウ
 class Grid(XUSelectGrid):
@@ -11,16 +11,21 @@ class Grid(XUSelectGrid):
     ITEM_W_ATTR = 'item_w'
     ITEM_H_ATTR = 'item_h'
 
-    def __init__(self, state:XUElem, item_tag:str):
-        super().__init__(state, item_tag, self.ROWS_ATTR, self.ITEM_W_ATTR, self.ITEM_H_ATTR)
+    def __init__(self, elem:XUElem, item_tag:str):
+        rows = elem.attr_int(self.ROWS_ATTR, 1)
+        item_w = elem.attr_int(self.ITEM_W_ATTR, 0)
+        item_h = elem.attr_int(self.ITEM_H_ATTR, 0)
+        super().__init__(elem, item_tag, rows, item_w, item_h)
 
 # リストウインドウ
 class List(XUSelectList):
     ITEM_W_ATTR = 'item_w'
     ITEM_H_ATTR = 'item_h'
 
-    def __init__(self, state:XUElem, item_tag:str):
-        super().__init__(state, item_tag, self.ITEM_W_ATTR, self.ITEM_H_ATTR)
+    def __init__(self, elem:XUElem, item_tag:str):
+        item_w = elem.attr_int(self.ITEM_W_ATTR, 0)
+        item_h = elem.attr_int(self.ITEM_H_ATTR, 0)
+        super().__init__(elem, item_tag, item_w, item_h)
 
 
 # デコレータを用意
@@ -29,8 +34,8 @@ class Decorator(XUTemplate.HasRef):
     def item(self, item_tag:str):
         def wrapper(bind_func:Callable[[Item,XUEvent], str|None]):
             # 登録用関数をジェネレート
-            def draw(state:XUElem, event:XUEvent):
-                return bind_func(Item(state), event)
+            def draw(elem:XUElem, event:XUEvent):
+                return bind_func(Item(elem), event)
             # 関数登録
             self.template.set_drawfunc(item_tag, draw)
         return wrapper
@@ -38,8 +43,8 @@ class Decorator(XUTemplate.HasRef):
     def grid(self, tag_name:str, item_tag:str):
         def wrapper(bind_func:Callable[[Grid,XUEvent], str|None]):
             # 登録用関数をジェネレート
-            def draw(state:XUElem, event:XUEvent):
-                return bind_func(Grid(state, item_tag), event)
+            def draw(elem:XUElem, event:XUEvent):
+                return bind_func(Grid(elem, item_tag), event)
             # 関数登録
             self.template.set_drawfunc(tag_name, draw)
         return wrapper
@@ -47,8 +52,8 @@ class Decorator(XUTemplate.HasRef):
     def list(self, tag_name:str, item_tag:str):
         def wrapper(bind_func:Callable[[List,XUEvent], str|None]):
             # 登録用関数をジェネレート
-            def draw(state:XUElem, event:XUEvent):
-                return bind_func(List(state, item_tag), event)
+            def draw(elem:XUElem, event:XUEvent):
+                return bind_func(List(elem, item_tag), event)
             # 関数登録
             self.template.set_drawfunc(tag_name, draw)
         return wrapper
