a2520deb6955122d85a6de605005cbca78aebfbe
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-19 18:38:15 +0900

    兵士調整

--- samples/DQ/field/system/npc.py
+++ samples/DQ/field/system/npc.py
@@ -14,14 +14,16 @@ class NPC_Data:
     color: int
     talk: str
 
+    ANIM_PAT = [0, 2, 0, 2]
+
     def setup(self):
         self.tile = XUXTilemap(1, 16, XURect(0, 0, 48, 64))
         self.draw_count = 0
 
     def draw(self, offset_x:int, offset_y:int):
-        pat = [1, 0, 1, 2]
         self.draw_count += 1
-        self.tile.draw_tile(self.x*16 + offset_x, self.y*16 + offset_y, pat[self.draw_count//10%len(pat)])
+        pat = self.ANIM_PAT[self.draw_count//15%len(self.ANIM_PAT)]
+        self.tile.draw_tile(self.x*16 + offset_x, self.y*16 + offset_y, pat)
 
 class NPC:
     npc_data = [
