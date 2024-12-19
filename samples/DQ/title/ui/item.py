e8ab01b02906a6167a13dbf6f66bb605b827d87e
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-19 18:15:29 +0900

    タイトルに工事中カラー設定

--- samples/DQ/title/ui/item.py
+++ samples/DQ/title/ui/item.py
@@ -5,7 +5,10 @@ from ui_common import system_font,draw_menu_cursor
 
 def menu_item(menu_item:XUSelectItem):
     area = menu_item.area
-    pyxel.text(area.x+6, area.y, menu_item.text, 7, system_font.font)
+    if menu_item.value == "工事中":
+        pyxel.text(area.x+6, area.y, menu_item.text, 15, system_font.font)
+    else:
+        pyxel.text(area.x+6, area.y, menu_item.text, 7, system_font.font)
 
     # カーソル追加
     if menu_item.selected:
