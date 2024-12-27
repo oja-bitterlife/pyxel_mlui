851d9bc215a25028ed318903dcdfa45325dab71f
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-27 17:43:28 +0900

    セピアとdititalを無くして赤を6階調に

--- xmlui/ext/pyxel_util.py
+++ xmlui/ext/pyxel_util.py
@@ -22,31 +22,27 @@ class PyxelPalette:
         # 16 デフォルトパレット
         self.palette = pyxel.colors.to_list()[:16]
 
-        # 15 デジタルパレット(16階調の0抜き)
-        self.digital_offset = len(self.palette)
-        self.palette += [0x800000, 0x008000, 0x808000, 0x000080, 0x800080, 0x008080, 0xc0c0c0, 0x808080, 0xff0000, 0x00ff00, 0xffff00, 0x0000ff, 0xff00ff, 0x00ffff, 0xffffff]
-
-        # 15 セピア
-        self.sepia_offset = len(self.palette)
-        self.palette += list(map(lambda v: int(v*0.93)<<16 | int(v*0.69)<<8 | int(v*0.4), [(v+1)*16 for v in range(15)]))
-
-        # 14 白黒
+        # 13 白黒。14番(238,238,238)は7番とかぶるのでナシで
         self.gray_offset = len(self.palette)
-        self.palette += [c<<16|c<<8|c for c in [17, 34, 51, 68, 85, 102, 119, 136, 153, 170, 187, 204, 221, 238]]
+        self.palette += [c<<16|c<<8|c for c in [17, 34, 51, 68, 85, 102, 119, 136, 153, 170, 187, 204, 221]]
 
-        # 180-1
+        # 216-1
         self.colors_offset = len(self.palette)
-        for r in [0, 60, 120, 180, 240]:  # 5
-            for g in [0, 48, 96, 144, 192, 240]:  # 6
-                for b in [0, 48, 96, 144, 192, 240]:  # 6
+        for r in [0, 48, 96, 128, 192, 255]:  # 6
+            for g in [0, 48, 96, 128, 192, 255]:  # 6
+                for b in [0, 48, 96, 128, 192, 255]:  # 6
                     # 黒は0が持ってるのでナシで
                     if r == g == b == 0:
                         continue
                     self.palette.append(r<<16 | g<<8 | b)
 
-        # 16+15+15+14+179 = 239
+        # 16+13+215 = 244
         self.free_offset = len(self.palette)
         self.reset()
+
+        # # チェック
+        # for i in range(8):
+        #     print(format(self.palette[self.pal_digital8[i]], "08x"))
 
     # システムにパレットを設定する
     def reset(self) -> Self:
@@ -55,38 +51,35 @@ class PyxelPalette:
 
     # パレット取得
     # -----------------------------------------------------
-    # セピアカラーパレット取得
-    @property
-    def pal_sepia16(self) -> list[int]:
-        return [0] + [self.sepia_offset + i for i in range(15)]
-
     # デジタルカラーパレット取得
     @property
     def pal_digital16(self) -> list[int]:
-        return [0] + [self.digital_offset + i for i in range(15)]
+        add_index = [36*3, 6*3, 36*3+6*3, 3, 36*3+3, 6*3+3, 36*4+6*4+4, 36*3+6*3+3, 36*5, 6*5, 36*5+6*5, 5, 36*5+5, 6*5+5, 36*5+6*5+5]
+        return [0] + [self.colors_offset + i-1 for i in add_index]
 
     @property
     def pal_digital8(self) -> list[int]:
-        return [0] + [self.digital_offset+8 + i for i in range(15)]
+        add_index = [36*5, 6*5, 36*5+6*5, 5, 36*5+5, 6*5+5, 36*5+6*5+5]
+        return [0] + self.pal_digital16[9:]
 
     # モノクロパレット取得
     @property
     def pal_gray16(self) -> list[int]:
-        return [0] + [self.gray_offset + i for i in range(14)] + [self.pal_white]
+        return [0] + [self.gray_offset + i for i in range(13)] + [7] + [self.pal_white]
 
     # 単色
     # -----------------------------------------------------
-    # 海外フリーゲームでよく使われるカラーキー
+    # 海外フリーゲームでよく使われるカラーキー(Magenta)
     @property
     def pal_magenta(self) -> int:
-        return self.digital_offset+12
+        return self.pal_digital8[5]
 
-    # フリーゲームや映像でよく使われるカラーキー
+    # フリーゲームや映像でよく使われるカラーキー(Green)
     @property
     def pal_green(self) -> int:
-        return self.digital_offset+9
+        return self.pal_digital8[2]
 
     # 白
     @property
     def pal_white(self) -> int:
-        return self.digital_offset+14
+        return self.pal_digital8[7]
