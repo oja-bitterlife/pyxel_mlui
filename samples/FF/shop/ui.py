79bd7acd515c3890ed3d7569fdab125a3a0bc0b6
oja-bitterlife <bitter.life.club@gmail.com>
2024-12-28 17:57:38 +0900

    コード整理

--- samples/FF/shop/ui.py
+++ samples/FF/shop/ui.py
@@ -9,50 +9,24 @@ from shop.buy_list import BuyList
 
 def ui_init(template:XUTemplate):
     shop_select = select.Decorator(template)
-    shot_text = text.Decorator(template)
+    shop_text = text.Decorator(template)
 
-    @shot_text.label("label")
+    # 基本ラベル
+    @shop_text.label("label")
     def label(label:text.Label, event:XUEvent):
         text = XUTextUtil.format_zenkaku(label.text, {"gil":user_data.gil})
         x, y = label.aligned_pos(system_font, text)
         pyxel.text(x, y, text, 7, system_font.font)
 
-
     # ショップ選択
     # *************************************************************************
+    # 基本選択物
     def shop_ui_item(shop_ui_item:XUSelectItem):
         area = shop_ui_item.area
         pyxel.text(area.x, area.y, shop_ui_item.text, 7, system_font.font)
 
         if shop_ui_item.selected:
             hand_cursor.draw(area.x, area.y+4)
-
-    def get_price(buy_menu:XUElem, buy_item:XUElem) -> int:
-        buy_num = XUSelectInfo(buy_menu.find_by_id("buy_num"))
-
-        price = int(buy_item.value) * int(buy_num.action)
-        match buy_num.action:  # 割引
-            case "4":
-                price -= price * 26 // 256
-            case "10":
-                price -= price * 51 // 256
-        return price
-
-    def shop_buy_item(shop_buy_item:XUSelectItem, parent_enable:bool):
-        buy_menu = shop_buy_item.find_parent_by_id("buy_menu")
-
-        # 商品名
-        area = shop_buy_item.area
-        pyxel.text(area.x, area.y, shop_buy_item.text, 7, system_font.font)
-
-        # お値段
-        price_text = XUTextUtil.format_zenkaku(get_price(buy_menu, shop_buy_item))
-        x,y = area.aligned_pos(system_font.text_width(price_text)+8, 0, XURect.Align.RIGHT)
-        pyxel.text(x, area.y, price_text, 7, system_font.font)
-
-        if shop_buy_item.selected and parent_enable:
-            hand_cursor.draw(area.x, area.y+4)
-
 
     # かう・うる・でるの選択
     # -----------------------------------------------------
@@ -89,6 +63,8 @@ def ui_init(template:XUTemplate):
                     # バトルへ
                     pass
 
+    # かうメニュー
+    # *************************************************************************
     # 個数を選択
     # -----------------------------------------------------
     @shop_select.list("buy_num", "shop_ui_item")
@@ -113,6 +89,37 @@ def ui_init(template:XUTemplate):
             shop_act_list = win.find_by_id("shop_act_list")
             shop_act_list.enable = True
 
+    # 購入できるアイテムのリスト
+    # -----------------------------------------------------
+    # 購入金額算出
+    def get_price(buy_menu:XUElem, buy_item:XUElem) -> int:
+        buy_num = XUSelectInfo(buy_menu.find_by_id("buy_num"))
+
+        price = int(buy_item.value) * int(buy_num.action)
+        match buy_num.action:  # 割引
+            case "4":
+                price -= price * 26 // 256
+            case "10":
+                price -= price * 51 // 256
+        return price
+
+    # 購入アイテムリスト
+    def shop_buy_item(shop_buy_item:XUSelectItem, parent_enable:bool):
+        buy_menu = shop_buy_item.find_parent_by_id("buy_menu")
+
+        # 商品名
+        area = shop_buy_item.area
+        pyxel.text(area.x, area.y, shop_buy_item.text, 7, system_font.font)
+
+        # お値段
+        price_text = XUTextUtil.format_zenkaku(get_price(buy_menu, shop_buy_item))
+        x,y = area.aligned_pos(system_font.text_width(price_text)+8, 0, XURect.Align.RIGHT)
+        pyxel.text(x, area.y, price_text, 7, system_font.font)
+
+        if shop_buy_item.selected and parent_enable:
+            hand_cursor.draw(area.x, area.y+4)
+
+    # 購入アイテム選択
     @shop_select.list("buy_list", "shop_buy_item")
     def buy_list(buy_list:select.List, event:XUEvent):
         for item in buy_list.items:
