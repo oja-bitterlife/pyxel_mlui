4429a891f0eeb0fccafbd193387bd05ac1848319
oja-bitterlife <bitter.life.club@gmail.com>
2025-01-13 16:02:04 +0900

    ターゲット名表示

--- samples/FF/battle/ui_result.py
+++ samples/FF/battle/ui_result.py
@@ -5,7 +5,7 @@ from xmlui.lib import text,debug
 from system import system_font
 
 from ui_common import get_world_clip
-from db import user_data
+from db import user_data,enemy_data
 from battle.data import BattleData
 
 def ui_init(xmlui:debug.DebugXMLUI[BattleData]):
@@ -16,4 +16,15 @@ def ui_init(xmlui:debug.DebugXMLUI[BattleData]):
         text = user_data.player_data[xmlui.data_ref.player_idx]["name"]
         pyxel.text(result_who.area.x, result_who.area.y, text, 7, system_font.font)
 
+    @result_text.label("result_target")
+    def result_target(result_target:text.XULabel, event:XUEvent):
+        target_idx = xmlui.data_ref.target[xmlui.data_ref.player_idx]
 
+        if target_idx < 0:
+            # パーティーアタック
+            target_idx = abs(target_idx)-1
+            target_name = user_data.player_data[target_idx]["name"]
+        else:
+            target_name = enemy_data.data[target_idx]["name"]
+
+        pyxel.text(result_target.area.x, result_target.area.y, target_name, 7, system_font.font)
