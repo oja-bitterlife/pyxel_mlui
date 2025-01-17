d819f7ae91fe63d7ab1f3bc1b467b828fff59224
oja-bitterlife <bitter.life.club@gmail.com>
2025-01-17 21:39:16 +0900

    CSV初回登録

--- xmlui/ext/db.py
+++ xmlui/ext/db.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3,csv
 
 # DB関係
 # ゲームではストレージに書き込まないのでmemory_dbに移して使う
@@ -21,6 +21,16 @@ class XUEMemoryDB:
         with open(db_path, "rb") as f:
             conn.deserialize(f.read())
         return XUEMemoryDB(conn).cursor()
+
+    # CSVを読み込んでメモリDB上にINSERT
+    @classmethod
+    def import_csv(cls, cursor:sqlite3.Cursor, csv_path:str, with_header:bool=True):
+        with open(csv_path, "r", encoding="utf-8") as f:
+            if with_header:
+                dict_reader = csv.DictReader(f)
+            else:
+                reader = csv.reader(f)
+
 
     # 閉じる、、、んだけど、たぶんcursor.connection.close()を呼ぶ気がする
     # :memory:なので閉じ忘れても特に問題はないはず
